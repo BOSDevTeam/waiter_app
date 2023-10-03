50355cc12d2ab9e611fa849f2cbd62a45f944ac3
Nwe Yi Aung <nweyiaung1990@gmail.com>
2023-10-03 15:42:04 +0630

    updated mac address code

--- lib/view/register_key.dart
+++ lib/view/register_key.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:waiter_app/provider/register_provider.dart';
 import 'package:waiter_app/view/data_downloading.dart';
 
 import '../value/app_color.dart';
 import '../value/app_string.dart';
-import 'package:mac_address/mac_address.dart';
 
 class RegisterKey extends StatefulWidget {
   const RegisterKey({super.key});
@@ -15,6 +15,8 @@ class RegisterKey extends StatefulWidget {
 }
 
 class _RegisterKeyState extends State<RegisterKey> {
+  static const channel = MethodChannel('com.example.getmac');
+
   @override
   void initState() {
     getMacAddress().then((value) {
@@ -26,21 +28,25 @@ class _RegisterKeyState extends State<RegisterKey> {
   }
 
   Future<String> getMacAddress() async {
-    return await GetMac.macAddress;
+    return await channel.invokeMethod('getMAC'); // a method that calls android code
   }
 
   String generateKey(String macAddress) {
-    List<String> list = macAddress.split(":");
-    String strMacAddress = list.join("");
-    String keyMacAddress = "blue${strMacAddress}ocean016";
-    String reverseKeyMacAddress = reverseString(keyMacAddress);
-    String firstKey = reverseKeyMacAddress.substring(0, 4);
-    String secondKey = reverseKeyMacAddress.substring(4, 8);
-    String thirdKey = reverseKeyMacAddress.substring(8, 12);
-    String fourthKey = reverseKeyMacAddress.substring(12, 16);
-    String fifthKey = reverseKeyMacAddress.substring(16, 20);
-    String sixKey = reverseKeyMacAddress.substring(20, 24);
-    String key = "$firstKey-$secondKey-$thirdKey-$fourthKey-$fifthKey-$sixKey";
+    String key = "";
+    if (macAddress.isNotEmpty) {
+      List<String> list = macAddress.split(":");
+      String strMacAddress = list.join("");
+      String keyMacAddress = "blue${strMacAddress}ocean016";
+      String reverseKeyMacAddress = reverseString(keyMacAddress);
+      String firstKey = reverseKeyMacAddress.substring(0, 4);
+      String secondKey = reverseKeyMacAddress.substring(4, 8);
+      String thirdKey = reverseKeyMacAddress.substring(8, 12);
+      String fourthKey = reverseKeyMacAddress.substring(12, 16);
+      String fifthKey = reverseKeyMacAddress.substring(16, 20);
+      String sixKey = reverseKeyMacAddress.substring(20, 24);
+      key = "$firstKey-$secondKey-$thirdKey-$fourthKey-$fifthKey-$sixKey";
+    }
+
     return key;
   }
 
